62053716e1518e25089a1f3efed9980e47ff43cb
Roohi-Naz <roohinaz700@gmail.com>
2024-05-27 19:45:08 +0500

    making new arrangment

---
+++ js 1.js
@@ -0,0 +1,5 @@
+
+alert("Welcome to my website")
+alert("Error!Please enter a valid code")
+alert("Welcome to js land \n Happy coding!")
+console.log("hello...I can run JS thriugh my web brower's consoles")
